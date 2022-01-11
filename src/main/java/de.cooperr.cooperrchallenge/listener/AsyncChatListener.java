3c4fac540f4cc356d85eef9c8be5bfe5b5a54973
Cooperr06 <colin.rinke@googlemail.com>
2022-01-11 18:49:04 +0100

    Added Chat Listener for formatting messages

---
+++ src/main/java/de.cooperr.cooperrchallenge/listener/AsyncChatListener.java
@@ -0,0 +1,26 @@
+package de.cooperr.cooperrchallenge.listener;
+
+import de.cooperr.cooperrchallenge.CooperrChallenge;
+import de.cooperr.cooperrchallenge.util.MinecraftListener;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+public class AsyncChatListener extends MinecraftListener {
+
+    public AsyncChatListener(CooperrChallenge plugin) {
+        super(plugin);
+    }
+
+    @EventHandler
+    public void onAsyncChat(AsyncChatEvent event) {
+        Player player = event.getPlayer();
+
+        event.message(player.displayName().style(Style.style(NamedTextColor.BLUE))
+                .append(Component.text(" » ", NamedTextColor.GRAY))
+                .append(event.originalMessage()));
+    }
+}
